510e9e213500f4bb53571ad62e15c9e1d68d016b
RachelNolan <mail@rachelnolan.com>
2014-10-21 17:16:07 +0100

    Players and computers choice is displayed along with result

--- src/game.js
+++ src/game.js
@@ -1,14 +1,16 @@
 var game = new RockPaperScissors
 var gest = {rock: new Rock, paper: new Paper, scissors: new Scissors}
+var computerGesture = null;
 
 var computersChoice = function() {
 	var gestures = [new Rock,  new Paper, new Scissors];
-	return gestures[Math.floor(Math.random()* gestures.length)];
+	computerGesture =  gestures[Math.floor(Math.random()* gestures.length)];
+	return computerGesture
 };
 
 var andTheWinnerIs = function(result) {
 	if(result === "draw")
-		$('h2').text("Snap, it's a " + (result));
+		$('h2').text("Snap, it's a " + (result) + "play again");
 	else
 		$('h2').text("The winner is " + (result.gesture));
 };
@@ -17,11 +19,14 @@ var playersChoice = function(choice) {
 	return gest[$(choice).data('gesture')];
 };
 
-$('img').on('click', function() {
-	andTheWinnerIs(game.compare(playersChoice(this), computersChoice()));	
-});
+	$('img').on('click', function() {
+		andTheWinnerIs(game.compare(playersChoice(this), computersChoice()));	
+	});
 
-// $('img').on('click', function() {
-// 	('#playersChoice').text("You chose " + (playersChoice(this))
-// 	('#computersChoice').text("The Computer chose " + (computersChoices))
-// });
+	$('img').on('click', function() {
+		$(".playersChoice").text("You chose " + ($(this).data('gesture')))
+	 });
+
+	$('img').on('click', function() {
+		$(".computersChoice").text("The Computer chose " + (computerGesture.gesture))
+	});
